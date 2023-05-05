cf9c0d72688fbf82fbd2c84f7d169a57a4f56516
giovannilampis <giolampis@hotmail.com>
2023-05-05 19:29:38 +0200

    dinamically create cards

--- src/assets/components/SingleCard.vue
+++ src/assets/components/SingleCard.vue
@@ -7,7 +7,11 @@ export default {
 </script>
 
 <template>
-    <div>
+    <div class="my-card">
+
+        <img :src="cardDetails.card_images[0].image_url_small" :alt="cardDetails.name">
+        <h5>{{ cardDetails.name }}</h5>
+        <h6>{{ cardDetails.archetype }}</h6>
         
     </div>
 </template>
