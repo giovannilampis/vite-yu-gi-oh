278e4d3bd82d66fe09682aaaec97f796c31e2fa7
giovannilampis <giolampis@hotmail.com>
2023-05-08 16:46:05 +0200

    modify api key to get

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,9 @@ export default{
       // console.log(response.data.data)
     //   this.store.arrayCards = apiData;
     // })
+
+    // modify callApi method
+    axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=3?name')
     }
   },
 }
